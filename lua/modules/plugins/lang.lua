cb29c7c5eb0acbd24413a340af211d9637b63427
Vaibhav Kumbhar <vkumbhar94@gmail.com>
2025-09-04 09:23:26 +0530

    feat(4.2.0): added my fixes

--- lua/modules/plugins/lang.lua
+++ lua/modules/plugins/lang.lua
@@ -42,4 +42,90 @@ lang["chrisbra/csv.vim"] = {
 	lazy = true,
 	ft = "csv",
 }
+lang["mfussenegger/nvim-jdtls"] = {
+	ft = "java",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"mfussenegger/nvim-dap",
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+		local function setup_jdtls()
+			local jdtls = require("jdtls")
+			local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+			local workspace_dir = vim.fn.stdpath("data") .. "/workspace/" .. project_name
+
+			local config = {
+				cmd = {
+					"java",
+					"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+					"-Dosgi.bundles.defaultStartLevel=4",
+					"-Declipse.product=org.eclipse.jdt.ls.core.product",
+					"-Dlog.protocol=true",
+					"-Dlog.level=ALL",
+					"-Xmx1g",
+					"--add-modules=ALL-SYSTEM",
+					"--add-opens",
+					"java.base/java.util=ALL-UNNAMED",
+					"--add-opens",
+					"java.base/java.lang=ALL-UNNAMED",
+					"-jar",
+					vim.fn.glob(
+						vim.fn.stdpath("data") .. "/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"
+					),
+					"-configuration",
+					vim.fn.stdpath("data") .. "/mason/packages/jdtls/config_mac",
+					"-data",
+					workspace_dir,
+				},
+				root_dir = jdtls.setup.find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+				settings = {
+					java = {
+						eclipse = { downloadSources = true },
+						configuration = { updateBuildConfiguration = "interactive" },
+						maven = { downloadSources = true },
+						implementationsCodeLens = { enabled = true },
+						referencesCodeLens = { enabled = true },
+						references = { includeDecompiledSources = true },
+						format = { enabled = true },
+					},
+					signatureHelp = { enabled = true },
+					completion = {
+						favoriteStaticMembers = {
+							"org.hamcrest.MatcherAssert.assertThat",
+							"org.hamcrest.Matchers.*",
+							"org.hamcrest.CoreMatchers.*",
+							"org.junit.jupiter.api.Assertions.*",
+							"java.util.Objects.requireNonNull",
+							"java.util.Objects.requireNonNullElse",
+							"org.mockito.Mockito.*",
+						},
+					},
+					sources = {
+						organizeImports = {
+							starThreshold = 9999,
+							staticStarThreshold = 9999,
+						},
+					},
+					codeGeneration = {
+						toString = {
+							template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+						},
+						useBlocks = true,
+					},
+				},
+				init_options = {
+					bundles = {},
+				},
+			}
+
+			jdtls.start_or_attach(config)
+		end
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "java",
+			callback = setup_jdtls,
+		})
+	end,
+}
 return lang
